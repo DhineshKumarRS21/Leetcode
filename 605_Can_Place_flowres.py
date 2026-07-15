32c62b4d26167bf08d986efad302f622e839c637
DhineshKumarRS21 <dhineshkumarrsof@gmail.com>
2026-07-15 14:38:46 +0530

    Create 605_Can_Place_flowres.py

---
+++ 605_Can_Place_flowres.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed.insert(0,0)
+        flowerbed.append(0)
+        for i in range(1,len(flowerbed)-1):
+            if flowerbed[i]==0 and flowerbed[i-1]==0 and flowerbed[i+1]==0:
+                flowerbed[i]=1
+                n-=1
+        print(n)
+        return True if n<=0 else False
